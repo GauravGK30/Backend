3ef894af7898e9cddc3102eaa0d7e90621ce9be0
Gaurav Kankuse <gauravkankuse18@gmail.com>
2025-07-29 20:21:55 +0530

    Initial commit

---
+++ NodeJS/module/hello.js
@@ -0,0 +1,17 @@
+// console.log("hello i am JS GK");
+
+const math = require('./math');
+// const math = require('');
+console.log(math);
+// console.log(math(2,5));
+
+console.log(math.addFn(2,5));
+console.log(math.subFn(5,1));
+
+
+
+// const {addFn,subFn} = require('./math');
+// console.log(addFn(5,1));
+// console.log(subFn(5,1));
+
+
