524b5fd6197f2aaa0aebecfd09735870161550da
Gaurav Kankuse <gauravkankuse18@gmail.com>
2025-07-29 23:16:17 +0530

    file handling new

--- NodeJS/module/file.js
+++ NodeJS/module/file.js
@@ -41,5 +41,7 @@ console.log(fs.statSync("./test.txt"));
 //make directory
 fs.mkdirSync('myDocs/a/abcd.txt',{recursive: true});
 
+
+//delete directory
 fs.rmdirSync('./myDocs',{recursive:true});
 // The node:fs module enables interacting with the file system in a way modeled on standard POSIX functions.
