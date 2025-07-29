fc00f83275ac40f46ae7c8353e6eb5af6ec31908
Gaurav Kankuse <gauravkankuse18@gmail.com>
2025-07-29 22:54:01 +0530

    file handling

---
+++ NodeJS/module/file.js
@@ -0,0 +1,26 @@
+const { log } = require('console');
+const fs = require('fs');
+
+
+// //sync
+// fs.writeFileSync('./test.txt',"hii GK");
+
+// //async
+// fs.writeFile('./test.txt',"Hello World",(err)=>{});
+
+//sync
+// const result = fs.readFileSync('./contact.txt','utf-8');
+// console.log(result);
+
+//async --->callback function needed
+fs.readFile('./contact.txt','utf-8',(err,result)=>{
+    if(err){
+        console.log("error",err);
+        
+    }else{
+        console.log(result);
+    }
+
+})
+
+// The node:fs module enables interacting with the file system in a way modeled on standard POSIX functions.
