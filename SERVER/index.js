aebd76e2c09bbffea839526fad2a57fbd0fabcbf
Gaurav Kankuse <gauravkankuse18@gmail.com>
2025-07-30 14:22:22 +0530

    http and url

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,20 +1,27 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 
 //always use non blocking request
 const myServer = http.createServer((req,res)=>{
     // console.log(req.headers);
 
+    if(req.url==='/favicon.ico') return res.end();
+
     const log = ` ${Date.now()}: ${req.url} new request received\n `;
+    const myUrl = url.parse(req.url,true);
+    console.log(myUrl);
     
     fs.appendFile('log.txt',log,(err,data)=>{
-        switch(req.url){
+        switch(myUrl.pathname){
             case '/':
                 res.end("Home page");
                 break;
             case '/about': 
-                res.end("Hii i am GK");
+                const username = myUrl.query.myName 
+                res.end(`hi,${username}`);
+                // res.end("Hii i am GK");
                 break;
 
             default:
