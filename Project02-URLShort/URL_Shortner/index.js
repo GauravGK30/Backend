910f3311d0b0fc0fd343ebff4894a7f91e8f5ebd
Gaurav Kankuse <gauravkankuse18@gmail.com>
2025-08-14 21:22:04 +0530

    main file

--- Project02-URLShort/URL_Shortner/index.js
+++ Project02-URLShort/URL_Shortner/index.js
@@ -10,7 +10,7 @@ const staticRoute = require("./routes/staticRouter");
 const userRoute = require("./routes/user")
 
 const app = express();
-const PORT = 8001;
+const PORT = 8005;
 
 //connection
 connectToMongoDB('mongodb://localhost:27017/short-url').then(()=>console.log("mongodb connected"));
